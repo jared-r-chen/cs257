76be0147bbadcc6f0de515779346d60967643dbd
Jared Chen <jared.chen@Jareds-MacBook-Pro.local>
2021-11-15 09:23:52 -0600

    commented queries.sql

--- webapp/queries.sql
+++ webapp/queries.sql
@@ -4,6 +4,7 @@ SELECT song_id, name, artist, highest_pos, streams
   WHERE UPPER(name) LIKE UPPER('%beg%')
   ORDER BY name;
 
+--add a quick comment about what this querie does when you have a minute
 SELECT song_id, name, artist, attributes_id, dancaeability, energy, loudness, speechiness, acousticness, liveness, tempo, duration, valence
   FROM songs, attributes
   WHERE UPPER(name) = UPPER(''' + modified_search + ''') AND song_id = attributes_id;
